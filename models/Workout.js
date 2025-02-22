84e66bfed563f883b83ca3d4d7717e8ab59b2f25
warder9 <76641104+warder9@users.noreply.github.com>
2025-02-22 12:22:42 +0500

    Delete models directory

--- models/Workout.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const WorkoutSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  name: String,
-  exercises: [{ name: String, sets: Number, reps: Number }],
-});
-
-module.exports = mongoose.model("Workout", WorkoutSchema);
