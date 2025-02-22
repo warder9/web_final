54855ada47effc94880f6df02fd26c764797a5bc
warder9 <76641104+warder9@users.noreply.github.com>
2025-02-22 11:37:52 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');  
-const UserSchema = new mongoose.Schema({  
-    username: { type: String, required: true },  
-    email: { type: String, required: true, unique: true },  
-    password: { type: String, required: true },  
-    role: { type: String, enum: ['user', 'admin'], default: 'user' }  
-});  
-module.exports = mongoose.model('User', UserSchema);  
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  email: { type: String, unique: true },
+  password: String,
+});
+
+module.exports = mongoose.model("User", UserSchema);
